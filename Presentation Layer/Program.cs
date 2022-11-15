828c99c1e873ea4c4b210ffb0fddcde348e51724
Meepo39-1 <emarin87@yahoo.com>
2022-11-15 12:57:08 +0200

    Add Frontend skeleton
    Did this to understand basic workflow in clean arhitecture project structure

--- Presentation Layer/Program.cs
+++ Presentation Layer/Program.cs
@@ -1,2 +1,59 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using Infrastructure;
+using Application;
+
+public class Program
+{
+    void LoginPage()
+    {
+        //render UI elements -> basic form
+        //sign in => read database
+        //sign up => write database
+    }
+    void UserPage()
+    {   /*components:
+         * User Data, ref to MovieLibrary (every movie has a watch me button),
+         * ref to FriendList
+        *WatchMovie Buttom ref to start cinemaRoom, ref to connect cinemaROOM link*/
+        //render UI elements => User data + Movie Library reference
+
+    }
+    void FriendListPage()
+    {
+        /* baisc SearchBar with username and some stats of that particular user
+         * Function to add or remove friends
+         */
+    }
+    void MovieLibrary()
+    {
+        /*same as FriendListPage, only that when you add a movie it will open a forum
+         * with: genre params, maybe pic, and the actual file content*/
+    } 
+    void WatchMovieAction()
+    {    //renders button
+         //movie upload field
+        //loads CinemaRoom Page
+    }
+
+    void CinemaRoomPage()
+    {
+        /* Components:
+         * Movie player in the center of the screen ->ref to MoviePLayer Infrastructure Object
+         * in top right corner, user Name ( option to hide)
+         * right portion of the page, the chat (transition to show only when an event is triggered
+         * (mouse, keyboard)
+         * buttons at the bottom of the screen:
+         * chat, get link, add friends (only for addmin), upload movie button (only for admin)-> functions in CinemaRoom Infrastructure object
+         * ID for cinemaRoom is generated trough userName that is gonna be unique
+         * every admin has one unique cinemaRoom
+         */   
+    }
+    
+   
+
+    static void Main()
+    {
+
+
+    }
+}
